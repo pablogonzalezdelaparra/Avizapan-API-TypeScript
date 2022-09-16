64ac428d8a6fc9d1f808f17b9b6b5c6cc22482e4
Pablo González de la Parra <a01745096@itesm.mx>
2022-09-15 21:16:57 -0500

    Connected database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/* Database */
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '157.230.12.204',
+      port: 3306,
+      username: 'admin',
+      password: 'avizapan',
+      database: 'avizapanDB',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
