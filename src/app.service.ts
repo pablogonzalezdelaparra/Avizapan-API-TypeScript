d93c291506e8d80e1b154c2a59464a14d5fe4909
Pablo González de la Parra <a01745096@itesm.mx>
2022-09-16 14:00:44 -0500

    Created Documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome! Please go to this <a href="https://avizapan-app-p6qc5.ondigitalocean.app/api">link</a> for more information!'
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Avizapan API')
+    .setDescription('Avizapan API is a REST API to be used for mobile app development in Android. Any questions or comments, please address them to "Team 4" on Slack.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
