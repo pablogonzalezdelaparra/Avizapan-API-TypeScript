046483736006f55751a184204a49ecc36906241d
Pablo González de la Parra <a01745096@itesm.mx>
2022-09-29 16:18:49 -0500

    Changed Cron expression

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { firstValueFrom, map, max } from 'rxjs';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
 
-  //@Cron('10 * * * * *')
+  @Cron(CronExpression.EVERY_12_HOURS)
   async runEvery24Hours() {
   //console.log(this.getHello());
   const url = 'https://api.open-meteo.com/v1/forecast?latitude=19.4271&longitude=-99.1276&hourly=temperature_2m';
@@ -24,7 +24,7 @@ export class AppService {
   }
   if (maxval > 23){
     let resp = {
-      "title": "Notificación",
+      "title": "Pablito",
       "description": "Que calor",
       "location": 99892,
       "duration": 64821,
